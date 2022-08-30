d7fe6316bb64ebd42a1f65c3d9ca5db242a3ca37
b-nagaj <bryce.nagaj@gmail.com>
2022-08-30 14:45:43 -0400

    Create the GenerateDataFile class

---
+++ lib/GenerateDataFile.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ostream>
+
+#pragma once
+
+class GenerateDataFile {
+
+    public:
+
+        std::string month;
+        std::string path;
+        std::string dataFilename;
+        std::string dataFilenamePath;
+        std::ofstream outfile;
+
+        GenerateDataFile();
+        void Prompt();
+        void Generate();
+
+};
