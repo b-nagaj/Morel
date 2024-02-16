226f4aafc2c086e57d19fe3a57a4369d1e50d0a5
b-nagaj <bryce.nagaj@gmail.com>
2024-02-15 20:33:05 -0500

    Remove Menu & ValidateChoice functions from Dialog

--- src/Dialog.cpp
+++ src/Dialog.cpp
@@ -13,28 +13,6 @@ Dialog::Dialog() {
 
 }
 
-void Dialog::Menu() {
-
-    std::cout << "\n\nEnter A Choice From The Menu\n";
-
-    std::cout << "\n\t1. Update List Of Expenses"
-              << "\n\t2. Generate Data Files"
-              << "\n\t3. Add a Transaction"
-              << "\n\t4. Delete a Transaction"
-              << "\n\t5. Generate A Report"
-              << "\n\t6. View A Report"
-              << "\n\t7. Quit";
-
-    std::cout << "\n\nChoice: ";
-    std::string choice;
-    std::cin >> choice;
-
-    if (ValidateChoice(choice)) {
-        option = std::stoi(choice);
-    }
-
-}
-
 void Dialog::WelcomeBanner() {
 
     GetUsername();
@@ -44,28 +22,6 @@ void Dialog::WelcomeBanner() {
               << "\n| |\\/| |/ _ \\| '__/ _ \\ |"
               << "\n| |  | | (_) | | |  __/ |"
               << "\n|_|  |_|\\___/|_|  \\___|_|";
-
-}
-
-bool Dialog::ValidateChoice(std::string c) {
-
-    bool isValid;
-
-    if (std::all_of(c.begin(), c.end(), ::isdigit) == false) {
-        std::cout << "\nERROR: The value you entered is not an integer\n\n<><><><><><><><><><><><><><>";
-        isValid = false;
-    }
-
-    else if (std::stoi(c) < (NUM_MENU_OPTIONS - (NUM_MENU_OPTIONS - 1)) || std::stoi(c) > NUM_MENU_OPTIONS) {
-        std::cout << "\nERROR: The value entered does not fall within range of " << std::to_string(NUM_MENU_OPTIONS - (NUM_MENU_OPTIONS - 1)) << "-" << std::to_string(NUM_MENU_OPTIONS) << "\n\n<><><><><><><><><><><><><><>";
-        isValid = false;
-    }
-
-    else {
-        isValid = true;
-    }
-
-    return isValid;
 
 }
 
