8a460f3d874be0800be82fa48455b30b036cb1b3
b-nagaj <bryce.nagaj@gmail.com>
2024-02-16 23:26:14 -0500

    Create new util class called 'Transaction'

---
+++ lib/Transaction.hpp
@@ -0,0 +1,32 @@
+#ifndef TRANSACTION
+#define TRANSACTION
+
+#include <string>
+
+#include "Date.hpp"
+
+class Transaction {
+    // attributes
+    private:
+        int userID;
+        std::string amount;
+        std::string category;
+        std::string transactionDate;
+    
+    // methods
+    private:
+        std::string GetTransactionDate();
+        void SetTransactionDate(Date date);
+        int GetUserID();
+        void SetUserID(int id);
+        std::string GetAmount();
+        void SetAmount(std::string a);
+        std::string GetCategory();
+        void SetCategory(std::string c);
+        std::string FormatAmountToDecimal(std::string value);
+    public:
+        Transaction(int id, double a, std::string c, std::string td);
+
+};
+
+#endif
