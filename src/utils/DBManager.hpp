3ad5fb66000f381dc7e3c9709d77e049f6026d25
b-nagaj <bryce.nagaj@gmail.com>
2024-02-25 21:00:12 -0500

    Create a new util class called 'DBManager'

---
+++ src/utils/DBManager.hpp
@@ -0,0 +1,27 @@
+#ifndef DB_MANAGER
+#define DB_MANAGER
+
+#include "../entities/Transaction.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <mysql/mysql.h>
+
+class DBManager {
+    // attributes
+    private:
+        MYSQL *connection;
+
+    //methods
+    private:
+        std::map<std::string, std::string> GetDBSecrets();
+    public:
+        bool Connect();
+        void CreateNewTransactions(Transaction *newTransactions, int numNewTransactions);
+        void Disconnect();
+};
+
+#endif
