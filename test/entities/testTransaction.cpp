617dd27fbf810724ab70e345850d55b5c9e80493
b-nagaj <bryce.nagaj@gmail.com>
2024-02-28 21:20:42 -0500

    Add comments to 'Transaction' test cases

--- test/entities/testTransaction.cpp
+++ test/entities/testTransaction.cpp
@@ -13,12 +13,13 @@ BOOST_AUTO_TEST_SUITE ( transactionTestSuite )
         BOOST_CHECK_EQUAL(transaction.GetUserID(), 0);
         BOOST_CHECK_EQUAL(transaction.GetAmount(), "");
         BOOST_CHECK_EQUAL(transaction.GetCategory(), "");
+        // validate that the transactionDate was converted to a timestamp
         BOOST_CHECK_EQUAL(transaction.GetTransactionDate(), "0000-00-00 00:00:00");
     }
 
     BOOST_AUTO_TEST_CASE( alternateTransactionContructor ) {
         int userID = 1;
-        Date date(2, 28, 2024);
+        Date date(2, 28, 2024); // initialize a new 'Date' object
         std::string amount = "1000.00";
         std::string category = "testCategory";
 
