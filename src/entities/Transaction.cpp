fdfe690c13c813954f7a862b88ffa302b14b3887
b-nagaj <bryce.nagaj@gmail.com>
2024-03-05 21:21:32 -0500

    Add method comments to 'Transaction' class

--- src/entities/Transaction.cpp
+++ src/entities/Transaction.cpp
@@ -1,5 +1,8 @@
 #include "Transaction.hpp"
 
+/**
+ * Default constructor that initializes each class attribute with '0' or ""
+ */
 Transaction::Transaction() {
     SetUserID(0);
     SetAmount("");
@@ -8,6 +11,14 @@ Transaction::Transaction() {
     SetTransactionDate(date);
 }
 
+/**
+ * Alternate constructor that initializes each class attribute based on input parameters
+ * 
+ * @param id value to set userID equal to
+ * @param a value to set amount equal to
+ * @param c value to set category equal to
+ * @param td value to set transactionDate equal to
+ */
 Transaction::Transaction(int id, std::string a, std::string c, Date td) {
     SetUserID(id);
     SetAmount(a);
@@ -17,36 +28,68 @@ Transaction::Transaction(int id, std::string a, std::string c, Date td) {
 
 // Getters
 
+/**
+ * Gets transaction userID
+ * 
+ * @return userID
+ */
 int Transaction::GetUserID() {
     return userID;
 }
 
+/**
+ * Gets transaction amount
+ * 
+ * @return amount
+ */
 std::string Transaction::GetAmount() {
     return amount;
 }
 
+/**
+ * Gets transaction category
+ * 
+ * @return category
+ */
 std::string Transaction::GetCategory() {
     return category;
 }
 
+/**
+ * Gets transaction date
+ * 
+ * @return transactionDate
+ */
 std::string Transaction::GetTransactionDate() {
     return transactionDate;
 }
 
 // Setters
 
+/**
+ * Sets transaction userID
+ */
 void Transaction::SetUserID(int id) {
     userID = id;
 }
 
+/**
+ * Sets transaction amount
+ */
 void Transaction::SetAmount(std::string a) {
     amount = a;
 }
 
+/**
+ * Sets transaction category
+ */
 void Transaction::SetCategory(std::string c) {
     category = c;
 }
 
+/**
+ * Sets transaction date
+ */
 void Transaction::SetTransactionDate(Date date) {
     transactionDate = date.FormatToTimestamp();
 }
