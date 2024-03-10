fb5e13fee68a3c5cd1c4c3a489e162ac8fdf6e6b
b-nagaj <bryce.nagaj@gmail.com>
2024-03-09 19:29:19 -0500

    Add method comments to 'User'

--- src/entities/User.cpp
+++ src/entities/User.cpp
@@ -1,13 +1,28 @@
 #include "User.hpp"
 
+/**
+ * alternate constructor that initializes a user's username
+ * 
+ * @param username a user's username
+ */ 
 User::User(std::string username) {
     SetUsername(username);
 }
 
+/** 
+ * retrieves a user's username
+ * 
+ * @return a user's username
+ */ 
 std::string User::GetUsername() {
     return username;
 }
 
+/**
+ * sets a user's username
+ * 
+ * @param u a user's username
+ */ 
 void User::SetUsername(std::string u) {
     username = u;
 }
