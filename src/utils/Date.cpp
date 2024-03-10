aeea78a8b4a9ea7fad0cf39eb16cfbfc8e5d9193
b-nagaj <bryce.nagaj@gmail.com>
2024-03-09 19:18:27 -0500

    Add method comments to 'Date'

--- src/utils/Date.cpp
+++ src/utils/Date.cpp
@@ -1,11 +1,24 @@
 #include "Date.hpp"
 
+/**
+ * Alternate constructor that initializes each class attribute based on input parameters
+ * 
+ * @param m a date's month
+ * @param d a date's day
+ * @param y a date's year
+ */ 
 Date::Date(int m, int d, int y) {
     month = m;
     day = d;
     year = y;
 }
 
+/**
+ * Formats a Date into a timestamp
+ * EX) 09-22-2000 -> 2000-09-22 00:00:00
+ * 
+ * @return a string that represent's a date in timestamp format
+ */ 
 std::string Date::FormatToTimestamp() {
     std::stringstream ss;
         ss << std::setfill('0');
