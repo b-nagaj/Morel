cd19b4cc9a681b3e15b67447f572fbdd27a33011
b-nagaj <bryce.nagaj@gmail.com>
2024-03-27 20:50:05 -0400

    Implement the DisplayTransaction() function

--- src/operations/DeleteTransaction.cpp
+++ src/operations/DeleteTransaction.cpp
@@ -3,6 +3,7 @@
 void DeleteTransaction::Delete() {
     GetTransaction();
     FindTransaction();
+    ConfirmOperation();
 }
 
 void DeleteTransaction::GetTransaction() {
@@ -15,7 +16,8 @@ bool DeleteTransaction::FindTransaction() {
     DBManager dbManager;
 
     if (dbManager.Connect()) {
-        dbManager.GetTransactionByAmount(transactionAmount);
+        result = dbManager.GetTransactionByAmount(transactionAmount);
+        mysql_free_result(result);
         dbManager.Disconnect();
     }
     else {
@@ -24,7 +26,41 @@ bool DeleteTransaction::FindTransaction() {
 }
 
 bool DeleteTransaction::ConfirmOperation() {
+    std::string confirmationResponse = "";
+    long numRows = mysql_num_rows(result);
 
+    if (numRows < 1) {
+        std::cout << "\nERROR: No transactions with an amount of "
+                  << transactionAmount
+                  << " exist";
+        return false;
+    }
+    else {
+        DisplayTransaction();
+
+        std::cout << "\nDelete the above transaction? (Y/N): ";
+        std::cin >> confirmationResponse;
+
+        if (confirmationResponse == "Y" || confirmationResponse == "y" ) {
+            return true;
+        }
+        else {
+            std::cout << "\n0 new Transaction(s) Added";
+            return false;
+        }
+    }
+}
+
+void DeleteTransaction::DisplayTransaction() {
+    MYSQL_ROW row;
+    
+    // display each transaction that matches the transactionAmount
+    while ((row = mysql_fetch_row(result)) != NULL) {
+        std::cout << "\nDate: " << (row[0] ? row[0] : "NULL");
+        std::cout << "\nAmount: $" << (row[1] ? row[1] : "NULL");
+        std::cout << "\nCategory: " << (row[2] ? row[2] : "NULL");
+        std::cout << std::endl;
+    }
 }
 
 void DeleteTransaction::DeleteTheTransaction() {
