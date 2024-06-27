89e0f60a38091712b426763e78aa62c033c05272
b-nagaj <bryce.nagaj@gmail.com>
2024-06-27 12:42:37 -0600

    Define an InfisicalService class

---
+++ src/services/infisical/InfisicalService.hpp
@@ -0,0 +1,12 @@
+#ifndef INFISICAL_SERVICE
+#define INFISICAL_SERVICE
+
+class InfisicalService {
+    // methods
+    public:
+        void Login();
+        void GetDBSecrets();
+        void WriteToEnvFile();
+};
+
+#endif
